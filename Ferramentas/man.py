76d3d51bd596ca62f7796b6b85c42186ff836a25
FELIPE <felipe.santos.santana.013@gmail.com>
2025-08-08 09:08:14 -0300

    Update man.py
    ainda não esta pronto

--- Ferramentas/man.py
+++ Ferramentas/man.py
@@ -1,7 +1,8 @@
 import drive2_teclado_matriz6x5
 import drive_oled_display128x64
 import time
-introdução="este é o manual do sistema e estes sao os principais comandos disponiveis por padrao no sistema: cat txt , cat py , mk arquivo txt , mk arquivo py , python , endboot"
+introdução="este é o manual do sistema sobre qual comando esta em duvida ?"
 
 drive_oled_display128x64.formatar_display(introdução)
-time.sleep(6)
+time.sleep(4)
+drive_oled_display128x64.display_clear()
